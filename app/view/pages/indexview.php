7de49a96f1098aa4aa3fdc5d13581f49f801fce5
sancloversan <christianpatrick86@yahoo.com>
2021-11-27 23:41:26 +0700

    Tambahan dan perbaikan pada halaman indexview.php

--- app/view/pages/indexview.php
+++ app/view/pages/indexview.php
@@ -2,21 +2,29 @@
 #halaman viewnya di sini
 #Yang diedit buat ditampilin ke client di bagian ini
 $title = "Sentiment Analyzer";
-echo "<p>Sentimen merupakan sentimen <strong>" . ($sentiment > 0 ? " Positif" : " Negatif") . "</strong>.</p></br>";
+if ($sentiment && $sentiment !== 0) {
+    echo "<p>Sentimen merupakan sentimen <strong>" . ($sentiment > 0 ? " Positif" : " Negatif") . "</strong>.</p></br>";
+}
 
-
-
+echo '
 <div class="sidenav">
+<form action="/" method="get">
     <div>
         <label>Single Review Analysis</label>
         <label for="singleReview">Enter single review below: </label>
-        <input type="text">
+        <input type="text" name="singleReview">
+        <button type="submit" >Submit</button>
     </div>
+</form>
 
+    <form action="/" method="post">
     <div>
         <label>Multiple Review Analysis</label>
         <label for="multipleReview">Upload .csv file</label>
-        <<input type="file" accept=".csv" />>
+        <input type="file" accept=".csv" name="fileReview"/>
+        <button type="submit" >Submit</button>
     </div>
+</form>
+    
 </div>
-
+';
