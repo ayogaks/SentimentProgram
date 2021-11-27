e76c088d43b0c3e0ba58bd591fb07dda98eeae5b
sancloversan <christianpatrick86@yahoo.com>
2021-11-27 23:44:47 +0700

    Tambahan dan perbaikan pada halaman indexview.php

--- app/view/404.php
+++ app/view/404.php
@@ -1,9 +1,8 @@
 <?php
 $title = "Not Found";
+?>
 
-echo '
 <body>
     <h1>404</h1>
     <h2>Not Found</h2>
 </body>
-';
--- app/view/pages/indexview.php
+++ app/view/pages/indexview.php
@@ -5,26 +5,25 @@ $title = "Sentiment Analyzer";
 if ($sentiment && $sentiment !== 0) {
     echo "<p>Sentimen merupakan sentimen <strong>" . ($sentiment > 0 ? " Positif" : " Negatif") . "</strong>.</p></br>";
 }
+?>
 
-echo '
+
 <div class="sidenav">
-<form action="/" method="get">
-    <div>
-        <label>Single Review Analysis</label>
-        <label for="singleReview">Enter single review below: </label>
-        <input type="text" name="singleReview">
-        <button type="submit" >Submit</button>
-    </div>
-</form>
+    <form action="/" method="get">
+        <div>
+            <label>Single Review Analysis</label>
+            <label for="singleReview">Enter single review below: </label>
+            <input type="text" name="singleReview">
+            <button type="submit">Submit</button>
+        </div>
+    </form>
 
     <form action="/" method="post">
-    <div>
-        <label>Multiple Review Analysis</label>
-        <label for="multipleReview">Upload .csv file</label>
-        <input type="file" accept=".csv" name="fileReview"/>
-        <button type="submit" >Submit</button>
-    </div>
-</form>
-    
+        <div>
+            <label>Multiple Review Analysis</label>
+            <label for="multipleReview">Upload .csv file</label>
+            <input type="file" accept=".csv" name="fileReview" />
+            <button type="submit">Submit</button>
+        </div>
+    </form>
 </div>
-';
