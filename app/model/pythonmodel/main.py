31f0784d87fd6f44b2ad03c47af765b5b1a8207d
sancloversan <christianpatrick86@yahoo.com>
2021-12-04 09:20:10 +0700

    [ADD] Tambah script main.py untuk handle input text atau file

---
+++ app/model/pythonmodel/main.py
@@ -0,0 +1,76 @@
+
+import preprocess as pp
+import pickle
+import sys
+import getopt
+
+with open('Forest.mod', 'rb') as f:
+    model = pickle.load(f)
+
+vectorizer = pickle.load(open("vector.pickel", "rb"))
+
+
+def remove_punctuation(text):
+    final = pp.teksCleaner(text)
+    final1 = pp.stopWords(final)
+    # final2 = pp.normalize(final1)
+    return final1
+
+
+def predictFromString(inputString):
+    if len(inputString):
+        review = remove_punctuation(inputString)
+
+        reviewList = []
+        reviewList.append(review)
+
+        predictions = model.predict(vectorizer.transform(reviewList))
+        print(predictions)
+
+    else:
+        print("py main.py -r <review>\npy main.py ( -f | --file <reviewFile.csv> )")
+        sys.exit(2)
+
+
+def predictFromFile(filename):
+    if len(filename) and ".csv" in filename:
+
+        import pandas as pd
+        df_new = pd.read_csv(filename)
+        df_new.head()
+
+        df_new = df_new.dropna(subset=['review'])
+        df_new['review'] = df_new['review'].apply(remove_punctuation)
+
+        predictions = model.predict(vectorizer.transform(df_new['review']))
+        print(predictions)
+
+    else:
+        print("py main.py -r <review>\npy main.py ( -f | --file <reviewFile.csv> )")
+        sys.exit(2)
+
+
+def main(argv):
+
+    try:
+        opts, args = getopt.getopt(argv, "hi", ["file="])
+    except:
+        print("py main.py -r <review>\npy main.py ( -f | --file <reviewFile.csv> )")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print("py main.py -r <review>\npy main.py ( -f | --file <reviewFile.csv> )")
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            print("is a file text")
+            inputReview = arg
+            predictFromFile(arg)
+        elif opt in ("-r"):
+            print("is a  text")
+            inputReview = arg
+            predictFromString(arg)
+
+
+if __name__ == "__main__":
+    main(sys.argv[:1])
